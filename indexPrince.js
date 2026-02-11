dddeb69b7180c11c1ae8560c5373f3f4e9ab3f1d
BrianP500 <156023663+BrianP500@users.noreply.github.com>
2026-02-11 11:34:34 -0600

    Add files via upload
    This is Prince's First mockup Files. a lot more work needs to be done

---
+++ indexPrince.js
@@ -0,0 +1,11 @@
+function scheduleEvent(){ //is a popup.
+    document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup(){
+    document.getElementById("popup").style.display = "none";
+}
+
+function selectTime(){
+    var time = document.getElementById("time").value;
+}
